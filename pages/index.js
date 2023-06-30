5a212f5c2f41b947a9823b918ec4af8adf84b071
kalburgimanjunath <1643278+kalburgimanjunath@users.noreply.github.com>
2023-06-30 19:55:04 +0530

    Initial commit

---
+++ pages/index.js
@@ -0,0 +1,29 @@
+export default function Home() {
+  const KundaliForm = () => {
+    return (
+      <div>
+        <div>Name </div>
+        <div>DOB </div>
+        <div>Time </div>
+        <div>Birth Place</div>
+        <div>Gotra</div>
+        <div>Rashi </div>
+        <div>Nakshtra</div>
+        <div>Charan </div>
+        <div>Gan </div>
+        <div>Nadi </div>
+        <div>Hight </div>
+        <div>Education </div>
+        <div>Work</div>
+        <div>Father Name</div>
+        <div>Address</div>
+        <div>Phone No</div>
+      </div>
+    );
+  };
+  return (
+    <div>
+      <KundaliForm />
+    </div>
+  );
+}
